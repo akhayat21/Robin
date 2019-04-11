34844aaba8335ae0d873e22028a97a2da871fa64
Jean Valderrama <jeanvalderrama@Jeans-MacBook-Pro.local>
2019-04-11 14:12:12 -0400

    created models for database

---
+++ models/Client.js
@@ -0,0 +1,73 @@
+module.exports = function(sequelize, DataTypes) {
+
+
+    var Client = sequelize.define("Client", {
+      clientName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [1,200]
+        }
+      },
+      contactLastname: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [1,200]
+        }
+      },
+      contactFirstName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [1,200]
+        }
+      },
+      phoneNumber: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        validate: {
+          len: [1,200]
+        }
+      },
+      
+      addressLine: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [1,200]
+        }
+      },
+      city: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [1,200]
+        }
+      },
+      state: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [1,200]
+        }
+      },
+      country: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [1,200]
+        }
+      },
+      Email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+          isEmail: true
+        }
+      },
+      
+    });
+    return Client;
+  };
---
+++ models/Sales.js
@@ -0,0 +1,42 @@
+module.exports = function(sequelize, DataTypes) {
+
+
+    var Sales = sequelize.define("Sales", {
+      SalesId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        validate: {
+          len: [1,100]
+        }
+      },
+      ClientId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        validate: {
+          len: [1,100]
+        }
+      },
+      SalesId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        validate: {
+          len: [1,100]
+        }
+      },
+      ProductId: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [1,200]
+        }
+      },
+      TransactionID: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [1,200]
+        }
+      }   
+    });
+    return Sales;
+  };
