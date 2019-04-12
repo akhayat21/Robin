795ca2cf3c2656e36f17136e3942e12181eaea05
Jean Valderrama <jeanvalderrama@Jeans-MacBook-Pro.local>
2019-04-12 13:48:04 -0400

    contractors name is now employee. I added  startDay, clockIn, and clockOut. in addition, I added comments to each table  and coloumn.

--- models/Employee.js
+++ models/Employee.js
@@ -2,6 +2,7 @@ module.exports = function(sequelize, DataTypes) {
 
  // Column for the employee database 
   var Employee = sequelize.define("Employee", {
+    // we want to store employee general information such as firstName, lastName, email. 
     firstName: {
       type: DataTypes.STRING,
       allowNull: false,
@@ -24,13 +25,15 @@ module.exports = function(sequelize, DataTypes) {
         isEmail: true
       }
     },
+    // this is to track the assignments for the employee teams.
     teamAssignment: {
       type: DataTypes.STRING,
       allowNull: false,
       validate: {
         len: [1,200]
       }
-    },
+    },  
+    // This is to track the employee weekly hours that  is gather formt the clock in and clock out data.
     weeklyHours: {
       type: DataTypes.INTEGER,
       allowNull: false,
@@ -38,6 +41,7 @@ module.exports = function(sequelize, DataTypes) {
         len: [1,100]
       },
     },
+    // This is to track the total sales of the employee.
     totalSales: {
       type: DataTypes.INTEGER,
       allowNull: false,
@@ -45,6 +49,7 @@ module.exports = function(sequelize, DataTypes) {
         len: [1,100]
       },
     },
+    // This clock in and out is  to track the employee hours so that we could add the total hours of the employee.
     clockIn: {
       type: DataTypes.INTEGER,
       allowNull: false,
@@ -58,7 +63,7 @@ module.exports = function(sequelize, DataTypes) {
       validate: {
         len: [1,100]
       },
-    },
+    },// This is to have a database information of the employeee start day with the company.
     startDay: {
       type: DataTypes.INTEGER,
       allowNull: false,
@@ -66,6 +71,7 @@ module.exports = function(sequelize, DataTypes) {
         len: [1,100]
       },
     },
+    // This for the user to have access to our features.
     adminAccess: {
       type: DataTypes.BOOLEAN,
       allowNull: false,
--- models/Product.js
+++ models/Product.js
@@ -1,6 +1,6 @@
 module.exports = function(sequelize, DataTypes) {
 
-// Column for the product database 
+// Column for the product database  with a general description of each product for us to track.
     var Product = sequelize.define("Product", {
       productName: {
         type: DataTypes.STRING,
@@ -11,25 +11,27 @@ module.exports = function(sequelize, DataTypes) {
       },
       productLine: {
         type: DataTypes.STRING,
-        allowNull: true,
+        allowNull: false,
         validate: {
           len: [1,200]
         }
       },
+      // This is for us to know the type of ventor that is using or giving the product.
       productVendor: {
         type: DataTypes.STRING,
-        allowNull: true,
+        allowNull: false,
         validate: {
           len: [1,200]
         }
       },
+      // This is to store a brief description of the product usage and special information.
       productDescription: {
         type: DataTypes.STRING,
-        allowNull: true,
+        allowNull: false,
         validate: {
           len: [1,265]
         }
-      },
+      },//  This is for us to track the quanity of stock in our inventory for us to get alert if we are running short in inventory.
       quantityinStock: {
         type: DataTypes.INTEGER,
         allowNull: false,
@@ -37,13 +39,14 @@ module.exports = function(sequelize, DataTypes) {
           len: [1,200]
         }
       },
+      // We want to store the whole sale price of the product for us to track profit and other financial information.
       wholesalePrice: {
         type: DataTypes.INTEGER,
         allowNull: false,
         validate: {
           len: [1,200]
         }
-      },
+      }, // This is the manufacturer sales retail price for us to use in financial information.
       msrp: {
         type: DataTypes.INTEGER,
         allowNull: false,
