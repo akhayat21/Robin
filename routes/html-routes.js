75f6bcddb64338735df8db07b64efe7098b889eb
Ky-Ann Long <kyannlong@gmail.com>
2019-04-14 17:22:29 -0400

    modifications to html routes

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,3 +1,5 @@
+var db = require("../models");
+
 // Requiring path to so we can use relative routes to our HTML files
 var path = require("path");
 
@@ -13,6 +15,24 @@ module.exports = function(app) {
   app.get("/manager", function(req, res) {
     res.render("mngrview-products");
   });
+
+  app.get("/manager/dashboard", function(req, res) {
+    db.Contractors.findAll({}).then(function(contractors) {
+      var weeklyHours = 0;
+      var totalSales = 0;
+      for (var i = 0; i < contractors.length; i++) {
+        weeklyHours += contractors[i].weeklyHours;
+        totalSales += contractors[i].totalSales;
+      }
+      console.log(weeklyHours);
+      console.log(totalSales);
+
+      db.Product.findAll({}).then(function(products) {
+        res.render("mngrview-dash");
+      });
+    });
+  });
+
 
   app.get("/login", function(req, res) {
     // If the user already has an account send them to the members page
