fc7d5bf59ab8edca7658dbd9c9f590a68f4c5c80
Akhayat <akhayat21@yahoo.com>
2019-04-15 12:08:12 -0400

    added modals to manager salestats

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -6,8 +6,12 @@ var isAuthenticated = require("../config/middleware/isAuthenticated");
 
 module.exports = function(app) {
 
-  app.get("/", function(req, res) {
-    res.render("index");
+  app.get("/signup", function(req, res) {
+    // If the user already has an account send them to the members page
+    if (req.user) {
+      res.redirect("/members");
+    }
+    res.sendFile(path.join(__dirname, "../public/signup.html"));
   });
 
   app.get("/manager/dash", function(req, res) {
