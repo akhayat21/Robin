89d4c40aebc26d980354878b2d185c3d36d30c8f
Akhayat <akhayat21@yahoo.com>
2019-04-15 11:01:54 -0400

    added modal funcionailty to contractor view

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -20,12 +20,29 @@ module.exports = function(app) {
     res.render("./mngerview/mngrview-contractors");
   });
   app.get("/manager/salestats", function(req, res) {
-    res.render("./mngerview/mngrview-salestats");
+    res.render("./mngerview/mngerview-salestats");
   });
-  app.get("/contractor/salestats", function(req, res) {
+  app.get("/contractor", function(req, res) {
     res.render("./contrview/contractorview");
   });
   
+  app.get("/manager/dash", function(req, res) {
+    db.Contractors.findAll({}).then(function(contractors) {
+      var weeklyHours = 0;
+      var totalSales = 0;
+      for (var i = 0; i < contractors.length; i++) {
+        weeklyHours += contractors[i].weeklyHours;
+        totalSales += contractors[i].totalSales;
+      }
+      console.log(weeklyHours);
+      console.log(totalSales);
+
+      db.Product.findAll({}).then(function(products) {
+        res.render("mngrview-dash");
+      });
+    });
+  });
+
   app.get("/login", function(req, res) {
     // If the user already has an account send them to the members page
     if (req.user) {
